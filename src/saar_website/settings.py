e8a4ecd3823da2916dd9f67c93df0f921bcdf13a
Ulrich KOUAKOU <Ulrich.KOUAKOU@SAARCI.LAN>
2024-08-14 18:45:38 +0000

    collecstatic, declaration sinistre auto, etc

--- src/saar_website/settings.py
+++ src/saar_website/settings.py
@@ -30,6 +30,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'saarci.assurance@gmail.com'
 EMAIL_HOST_PASSWORD = 'opkt koyt lgqu rjeb'
+# EMAIL_PORT = 465
+# EMAIL_USE_SSL = True
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Equipe Saar CI <noreply@saarci.com>'
@@ -139,6 +141,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'saar_website/static'
 ]
+
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
